487c8056f4acb365e77ce6c802eaf24e4d1f85f9
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-03-22 20:09:08 +0100

    Next shot at git-lfs workaround

--- conf.py
+++ conf.py
@@ -62,5 +62,10 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 # Implement build logic on RTD servers
 if read_the_docs_build:
-    import git_lfs
-    git_lfs.fetch(os.getcwd())
+    # Workaround to install and execute git-lfs on Read the Docs
+    if not os.path.exists('./git-lfs'):
+        os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.1/git-lfs-linux-amd64-v2.7.1.tar.gz')
+        os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz')
+        os.system('./git-lfs install')  # make lfs available in current repository
+        os.system('./git-lfs fetch')  # download content from remote
+        os.system('./git-lfs checkout')  # make local files to have the real content on them
